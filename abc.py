3e92916b091224de5d4fbfa842b80495232e791f
KhadijaAbbasi <85893701+KhadijaAbbasi@users.noreply.github.com>
2021-07-27 16:12:32 +0500

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+def display():
+    print("Name: Khadija")
+    print("Reg_no: 036")
+    print("Class: BSE-5")
+    print("Section: A")
+    
+    
+
+def main():
+    display()
+if __name__== "__main__":
+    main()
+
